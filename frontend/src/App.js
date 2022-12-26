00253b687562777439502767be794eceacffab5d
luanpnm <luanpnm@gmail.com>
2022-12-26 22:04:34 +0700

    02-product-details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,30 @@
+import data from './data';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <a href="/">jayShop</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product">
+              <img src={product.image} alt={product.name} />
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
